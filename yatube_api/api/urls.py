a60645ae788dd80d9b43d80e84280993d73a3ba4
Ilya Petrukhin <Tvules@mail.ru>
2022-06-29 16:33:44 +0400

    Creating routers for viewsets.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import PostViewSet, CommentsViewSet, GroupViewSet, FollowViewSet
+
+router = routers.DefaultRouter()
+router.register('posts', PostViewSet)
+router.register(
+    r'posts/(?P<post_id>[\d]+)/comments',
+    CommentsViewSet,
+    basename='comment',
+)
+router.register('groups', GroupViewSet)
+router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls.jwt')),
 ]
