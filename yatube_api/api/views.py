c008ec928707aa2a4f6daca0444ac2e035c65c81
Ilya Petrukhin <Tvules@mail.ru>
2022-07-01 18:27:07 +0400

    Deleting unused import.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import (
-    viewsets, permissions, filters, mixins, pagination
+    viewsets, permissions, filters, pagination
 )
 
 from .mixins import FollowViewSetBase
